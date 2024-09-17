199352ec6fb53c17ef9da8f224cc248f37d587ce
dbqudgns <dbqudgns4632@gmail.com>
2024-09-17 23:48:07 +0900

    로그인 처리된 사용자 확인용 인터셉터 생성

---
+++ src/main/java/Project/Travel_Diary/config/WebConfig.java
@@ -0,0 +1,21 @@
+package Project.Travel_Diary.config;
+
+import Project.Travel_Diary.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/user/add", "/login", "/logout", "/css/**", "/image/**");
+
+    }
+
+}
